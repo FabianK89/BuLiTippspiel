e232b974589cd15e70291df2e7c818b9761d3bdd
FabianK89 <fabian.kiesl@gmail.com>
2017-10-24 16:48:14 +0200

    created navbar dummy

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../../services/navbar.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [NavbarService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public nav: NavbarService ) { }
 
   ngOnInit() {
+    this.nav.hide();
   }
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../../services/navbar.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [NavbarService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( public nav: NavbarService ) { }
 
   ngOnInit() {
+    this.nav.hide();
   }
 
 }
